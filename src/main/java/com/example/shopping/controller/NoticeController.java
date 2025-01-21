7e4485411dc377fa1aeee500b17cd1685cfe044c
gram <gram@DESKTOP-E54V6V8>
2025-01-21 13:11:35 +0900

    2024-04-19 (마무리)

--- src/main/java/com/example/shopping/controller/NoticeController.java
+++ src/main/java/com/example/shopping/controller/NoticeController.java
@@ -1,12 +1,22 @@
 package com.example.shopping.controller;
 
+import com.example.shopping.dto.NoticeDTO;
+import com.example.shopping.service.NoticeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class NoticeController {
+    private final NoticeService noticeService;
     @GetMapping("/notice")
-    public String notice_index(){
+    public String notice_index(Model model){
+        List<NoticeDTO> noticeDTOList = noticeService.findAll();
+        model.addAttribute("noticeList", noticeDTOList);
 
         return "/notice/index";
     }
